a210871c244b680f56ec8c6bcbc08145255d05f6
Rehno Lindeque <errantkid@gmail.com>
2012-01-11 13:31:14 -0800

    Updated glQuery to the latest version

--- static/lib/glquery/glquery.js
+++ static/lib/glquery/glquery.js
@@ -773,7 +773,7 @@ var glQuery = (function() {
           // }
           // context.linkProgram(shaderProgram);
           logError("Internal Error: shaderProgram(WebGLShader, ...) is not yet supported by glQuery.");
-          return
+          return;
         }
         // Add an index to the shader program (We can't index the shader locations by shader program 
         // when the shader program is an instance of WebGLProgram because toString simply gives
@@ -791,7 +791,11 @@ var glQuery = (function() {
           }
           for (var i = 0; i < activeUniforms; ++i) {
             var uniform = context.getActiveUniform(shaderProgram, i);
-            locations.uniforms[uniform.name] = context.getUniformLocation(shaderProgram, uniform.name);
+            locations.uniforms[uniform.name] = { 
+              location: context.getUniformLocation(shaderProgram, uniform.name),
+              size: uniform.size,
+              type: uniform.type
+            };
           }
         }
         // Add shader program associations to tags
@@ -914,8 +918,12 @@ var glQuery = (function() {
       }
       // Store the command
       for (var i = 0; i < selector.length; ++i) {
-        var commandsStruct = (typeof tagCommands[selector[i]] === 'undefined'? (tagCommands[selector[i]] = {}) : tagCommands[selector[i]]);
-        commandsStruct[command.uniform] = args;
+        var commandsStruct = (typeof tagCommands[selector[i]] === 'undefined'? (tagCommands[selector[i]] = {}) : tagCommands[selector[i]]),
+        uniformTable = commandsStruct[command.uniform];
+        if(uniformTable == null)
+          uniformTable = commandsStruct[command.uniform] = {};
+        //commandsStruct[command.uniform] = args;
+        uniformTable[args[0]] = args.slice(1);
       }
     },
     // insert: 9
@@ -988,21 +996,51 @@ var glQuery = (function() {
       logDebug("eval command: vertexAttrib4");
     },
     // uniform: 8
-    function(context, renderState, args) {
-      logDebug("eval command: uniform");
-      var uniformFunctions = [
-        WebGLRenderingContext.prototype.uniform1f,
-        WebGLRenderingContext.prototype.uniform2f,
-        WebGLRenderingContext.prototype.uniform3f
-      ];
-      var locations = (renderState.shaderProgram != null? shaderLocations[renderState.shaderProgram._glquery_id] : null);
-      if (locations != null) {
-        var uniformLocation = (typeof args[0] == 'number'? args[0] : locations.uniforms[args[0]]);
-        if (uniformLocation != null) {
-          uniformFunctions[args.length - 2].apply(context, [uniformLocation].concat(args.slice(1)));
+    (function() {
+      var uniformEval = {};
+      uniformEval[gl.FLOAT] = WebGLRenderingContext.prototype.uniform1f;
+      uniformEval[gl.FLOAT_VEC2] = WebGLRenderingContext.prototype.uniform2f;
+      uniformEval[gl.FLOAT_VEC3] = WebGLRenderingContext.prototype.uniform3f;
+      uniformEval[gl.FLOAT_VEC4] = WebGLRenderingContext.prototype.uniform4f;
+      uniformEval[gl.INT] = WebGLRenderingContext.prototype.uniform1i;
+      uniformEval[gl.INT_VEC2] = WebGLRenderingContext.prototype.uniform2i;
+      uniformEval[gl.INT_VEC3] = WebGLRenderingContext.prototype.uniform3i;
+      uniformEval[gl.INT_VEC4] = WebGLRenderingContext.prototype.uniform4i;
+      uniformEval[gl.BOOL] = WebGLRenderingContext.prototype.uniform1i;
+      uniformEval[gl.BOOL_VEC2] = WebGLRenderingContext.prototype.uniform2i;
+      uniformEval[gl.BOOL_VEC3] = WebGLRenderingContext.prototype.uniform3i;
+      uniformEval[gl.BOOL_VEC4] = WebGLRenderingContext.prototype.uniform4i;
+      uniformEval[gl.FLOAT_MAT2] = function(location, value, transpose) { this.uniformMatrix2fv(location, transpose != null? transpose : false, value); };
+      uniformEval[gl.FLOAT_MAT3] = function(location, value, transpose) { this.uniformMatrix3fv(location, transpose != null? transpose : false, value); };
+      uniformEval[gl.FLOAT_MAT4] = function(location, value, transpose) { this.uniformMatrix4fv(location, transpose != null? transpose : false, value); };
+      //uniformEval[gl.SAMPLER_2D] =
+      //uniformEval[gl.SAMPLER_CUBE] = 
+
+      return function(context, renderState, args) {
+        logDebug("eval command: uniform");
+        // TODO: Detect uniformMatrix (supplied without the special transpose flag?)
+        // I.e. use attributes stored by getLocation?
+        var locations = (renderState.shaderProgram != null? shaderLocations[renderState.shaderProgram._glquery_id] : null);
+        if (locations != null) {
+          // TODO: How to get the uniform info if it is already given as a UniformLocation object?
+          //       Can we query/cache it in a fast way?
+          //       Probably need to set up benchmark for this...
+          for (var uniformName in args) { // args is a table of uniforms
+            /* Not supported: WebGLUniformLocation - (because uniform locations are specific to some shader program)
+            if (key instanceof WebGLUniformLocation) {
+              console.log(uniformLocation);	
+              uniformLocation = args[0];
+              // TODO: uniformInfo = locations.uniforms[args[0]];
+            }*/
+            var uniformInfo = locations.uniforms[uniformName],
+            uniformLocation = uniformInfo.location,
+            uniformArgs = args[uniformName];
+            if (uniformLocation != null && uniformInfo != null)
+              uniformEval[uniformInfo.type].apply(context, [uniformLocation].concat(uniformArgs));
+          }
         }
-      }
-    }
+      };
+    })()
   ];
 
   //assert(commandDispatch.length === command.length, "Internal Error: Number of commands in commandDispatch is incorrect.");
--- static/lib/glquery/glquery.math.module.js
+++ static/lib/glquery/glquery.math.module.js
@@ -7,8 +7,125 @@ var glQueryMath = new (function() {
 "use strict";
 
 // Define a local copy of glQuery
-var glQueryMath = this != null? this : window;
+var glQueryMath = this != null? this : window,
+MathMemoryPool = {
+  matrix4: [
+    [0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0], 
+    [0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0],
+    [0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0], 
+    [0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0]],
+  matrix3: [
+    [0.0,0.0,0.0, 0.0,0.0,0.0, 0.0,0.0,0.0], 
+    [0.0,0.0,0.0, 0.0,0.0,0.0, 0.0,0.0,0.0],
+    [0.0,0.0,0.0, 0.0,0.0,0.0, 0.0,0.0,0.0]],
+  matrix2: [
+    [0.0,0.0, 0.0,0.0], 
+    [0.0,0.0, 0.0,0.0]],
+  vector4: [[0.0,0.0,0.0,0.0], [0.0,0.0,0.0,0.0], [0.0,0.0,0.0,0.0], [0.0,0.0,0.0,0.0]],
+  vector3: [[0.0,0.0,0.0], [0.0,0.0,0.0], [0.0,0.0,0.0], [0.0,0.0,0.0]],
+  vector2: [[0.0,0.0], [0.0,0.0], [0.0,0.0], [0.0,0.0]]
+};
+
+glQueryMath.vec2 = {};
+glQueryMath.vec2.add = function(a,b,dest) {
+  dest[0] = a[0] + b[0]; 
+  dest[1] = a[1] + b[1];
+  return dest;
+};
+glQueryMath.vec2.subtract = function(a,b,dest) {
+  dest[0] = a[0] - b[0];
+  dest[1] = a[1] - b[1];
+  return dest;
+};
+glQueryMath.vec2.neg = function(a,dest) {
+  dest[0] = -a[0];
+  dest[1] = -a[1];
+};
+glQueryMath.vec2.dot = function(a,b) {
+  return a[0] * b[0] + a[1] * b[1];
+};
+glQueryMath.vec2.length = function(a) {
+  return Math.sqrt(a[0] * a[0] + a[1] * a[1]);
+};
+
+glQueryMath.vec3 = {};
+glQueryMath.vec3.add = function(dest,a,b) {
+  dest[0] = a[0] + b[0]; 
+  dest[1] = a[1] + b[1];
+  dest[2] = a[2] + b[2]; 
+  return dest;
+};
+glQueryMath.vec3.sub = function(dest,a,b) {
+  dest[0] = a[0] - b[0];
+  dest[1] = a[1] - b[1];
+  dest[2] = a[2] - b[2];
+  return dest;
+};
+glQueryMath.vec3.mul = function(dest,a,b) {
+  dest[0] = a[0] * b;
+  dest[1] = a[1] * b;
+  dest[2] = a[2] * b;
+  return dest;
+};
+glQueryMath.vec3.div = function(dest,a,b) {
+  dest[0] = a[0] / b;
+  dest[1] = a[1] / b;
+  dest[2] = a[2] / b;
+  return dest;
+};
+glQueryMath.vec3.neg = function(dest,a) {
+  dest[0] = -a[0];
+  dest[1] = -a[1];
+  dest[2] = -a[2];
+  return dest;
+};
+glQueryMath.vec3.dot = function(a,b) {
+  return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+};
+glQueryMath.vec3.cross = function(dest,a,b) {
+  dest[0] = a[1] * b[2] - a[2] * b[1];
+  dest[1] = a[2] * b[0] - a[0] * b[2];
+  dest[2] = a[0] * b[1] - a[1] * b[0];
+  return dest;
+};
+glQueryMath.vec3.length = function(a) {
+  return Math.sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
+};
+glQueryMath.vec3.normalize = function(dest,a) {
+  return glQueryMath.vec3.div(dest, a, glQueryMath.vec3.length(a));
+};
+
+glQueryMath.vec4 = {};
+glQueryMath.vec4.add = function(a,b,dest) {
+  dest[0] = a[0] + b[0]; 
+  dest[1] = a[1] + b[1];
+  dest[2] = a[2] + b[2]; 
+  dest[3] = a[3] + b[3];
+  return dest;
+};
+glQueryMath.vec4.subtract = function(a,b,dest) {
+  dest[0] = a[0] - b[0];
+  dest[1] = a[1] - b[1];
+  dest[2] = a[2] - b[2];
+  dest[3] = a[3] - b[3];
+  return dest;
+};
+glQueryMath.vec4.neg = function(a,dest) {
+  dest[0] = -a[0];
+  dest[1] = -a[1];
+  dest[2] = -a[2];
+  dest[3] = -a[3];
+};
+glQueryMath.vec4.dot = function(a,b) {
+  return a[0] * b[0] + a[1] * b[1] + a[2] * b[2] + a[3] * b[3];
+};
+glQueryMath.vec4.length = function(a) {
+  return Math.sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2] + a[3] * a[3]);
+};
+
 // Module for setting 3x3 matrix values
+
+// Axis-angle rotation matrix using the right hand rule
 glQueryMath.setMatrix3AxisRotation = function(axis, angle) {
   var
   // Convert rotation to quaternion representation
@@ -29,6 +146,7 @@ glQueryMath.setMatrix3AxisRotation = function(axis, angle) {
     2 * (xz + yw),     2 * (y*z - xw),    1 - 2 * (xx + yy)];
 };
 
+// Matrix identity
 glQueryMath.setMatrix3Identity = function() {
   return [
     1.0, 0.0, 0.0,
@@ -37,6 +155,8 @@ glQueryMath.setMatrix3Identity = function() {
 };
 
 // Module for setting 4x4 matrix values
+
+// Axis-angle rotation matrix using the right hand rule
 glQueryMath.setMatrix4AxisRotation = function(axis, angle) {
   var
   // Convert rotation to quaternion representation
@@ -58,6 +178,7 @@ glQueryMath.setMatrix4AxisRotation = function(axis, angle) {
     0,                 0,                 0,                  1];
 };
 
+// Matrix identity
 glQueryMath.setMatrix4Identity = function() {
   return [
     1.0, 0.0, 0.0, 0.0,
@@ -66,6 +187,60 @@ glQueryMath.setMatrix4Identity = function() {
     0.0, 0.0, 0.0, 1.0];
 };
 
+glQueryMath.setMatrix4Rows = function(r0, r1, r2, r3) {
+  return [].concat(r0, r1, r2, r3);
+}
+
+glQueryMath.setMatrix4Columns = function(c0, c1, c2, c3) {
+  return [
+    c0[0], c1[0], c2[0],c3[0],
+    c0[1], c1[1], c2[1],c3[1],
+    c0[2], c1[2], c2[2],c3[2],
+    c0[3], c1[3], c2[3],c3[3]
+  ];
+}
+
+// Right-handed orthogonal projection matrix
+glQueryMath.setMatrix4Ortho = function(left, right, bottom, top, near, far) {
+  var x = left - right,
+  y = bottom - top,
+  z = near - far;
+  return [
+    -2.0 / x,           0.0,                0.0,              0.0,
+    0.0,               -2.0 / y,            0.0,              0.0,
+    0.0,                0.0,                2.0 / z,          0.0,
+    (left + right) / x, (top + bottom) / y, (far + near) / z, 1.0
+  ];
+};
+
+// Right-handed look-at matrix
+glQueryMath.setMatrix4LookAt = function(eye, target, up) {
+  // TODO: See if it would be more efficient to try and build the matrix
+  //       by rows instead of by columns as is done presently
+  var x = MathMemoryPool.vector4[0], 
+  y = MathMemoryPool.vector4[1],
+  z = MathMemoryPool.vector4[2],
+  w = MathMemoryPool.vector4[3];
+
+  glQueryMath.vec3.sub(z, eye, target);
+  glQueryMath.vec3.cross(x, up, z);
+
+  // (probably best to normalize z and x after cross product for best numerical accuracy)
+  glQueryMath.vec3.normalize(x, x);
+  glQueryMath.vec3.normalize(z, z);
+
+  // (no need to normalize y because x and z was already normalized)
+  glQueryMath.vec3.cross(y, z, x);
+  
+  x[3] = -glQueryMath.vec3.dot(x, eye);
+  y[3] = -glQueryMath.vec3.dot(y, eye);
+  z[3] = -glQueryMath.vec3.dot(z, eye);
+  w[0] = 0.0;
+  w[1] = 0.0;
+  w[2] = 0.0;
+  w[3] = 1.0;
+  return glQueryMath.setMatrix4Columns(x,y,z,w);
+};
 
 // Extend glQuery if it is defined
 if (glQuery != null)
