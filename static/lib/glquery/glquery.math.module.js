e7fab07ae2bd6d98830a55c8a48881d6628da747
Rehno Lindeque <errantkid@gmail.com>
2012-01-12 23:36:35 -0800

    Update the glQuery math module (fix axis-angle rotations)

--- static/lib/glquery/glquery.math.module.js
+++ static/lib/glquery/glquery.math.module.js
@@ -138,12 +138,12 @@ glQueryMath.setMatrix3AxisRotation = function(axis, angle) {
   w = Math.cos(halfAngle),
   // Convert quaternion to matrix representation
   xx = x*x, xy = x*y, xz = x*z, xw = x*w,
-  yy = y*y, yw = y*w,
+  yy = y*y, yz = y*z, yw = y*w,
   zz = z*z, zw = z*w;
   return [
     1 - 2 * (yy + zz), 2 * (xy + zw),     2 * (xz - yw),
-    2 * (xy - zw),     1 - 2 * (xx + zz), 2 * (zy + xw),
-    2 * (xz + yw),     2 * (y*z - xw),    1 - 2 * (xx + yy)];
+    2 * (xy - zw),     1 - 2 * (xx + zz), 2 * (yz + xw),
+    2 * (xz + yw),     2 * (yz - xw),     1 - 2 * (xx + yy)];
 };
 
 // Matrix identity
@@ -169,12 +169,12 @@ glQueryMath.setMatrix4AxisRotation = function(axis, angle) {
   w = Math.cos(halfAngle),
   // Convert quaternion to matrix representation
   xx = x*x, xy = x*y, xz = x*z, xw = x*w,
-  yy = y*y, yw = y*w,
+  yy = y*y, yz = y*z, yw = y*w,
   zz = z*z, zw = z*w;
   return [
     1 - 2 * (yy + zz), 2 * (xy + zw),     2 * (xz - yw),      0,
-    2 * (xy - zw),     1 - 2 * (xx + zz), 2 * (zy + xw),      0,
-    2 * (xz + yw),     2 * (y*z - xw),    1 - 2 * (xx + yy),  0,
+    2 * (xy - zw),     1 - 2 * (xx + zz), 2 * (yz + xw),      0,
+    2 * (xz + yw),     2 * (yz - xw),     1 - 2 * (xx + yy),  0,
     0,                 0,                 0,                  1];
 };
 
