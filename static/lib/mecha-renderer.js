25d94ecd69af18bbf053fff9645319d5a7547be2
Rehno Lindeque <errantkid@gmail.com>
2012-01-19 17:52:58 -0800

    Removed the last remaining SceneJS and gl-matrix files/code from the build

--- static/lib/mecha-renderer.js
+++ static/lib/mecha-renderer.js
@@ -8,7 +8,7 @@ mecha.renderer =
 
   "use strict";
 
-  var createScene, exports, gl, lookAtToQuaternion, math_degToRad, math_invsqrt2, math_radToDeg, math_sqrt2, modelArguments, modelRotate, modelShaders, modifySubAttr, orbitLookAt, orbitLookAtNode, recordToVec3, recordToVec4, runScene, state, vec3ToRecord, vec4ToRecord, zoomLookAt, zoomLookAtNode;
+  var createScene, exports, gl, math_degToRad, math_invsqrt2, math_radToDeg, math_sqrt2, modelArguments, modelRotate, modelShaders, runScene, state;
 
   math_sqrt2 = Math.sqrt(2.0);
 
@@ -50,126 +50,6 @@ mecha.renderer =
       fs: null
     },
     rotation: [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
-  };
-
-  modifySubAttr = function(node, attr, subAttr, value) {
-    var attrRecord;
-    attrRecord = node.get(attr);
-    attrRecord[subAttr] = value;
-    return node.set(attr, attrRecord);
-  };
-
-  recordToVec3 = function(record) {
-    return [record.x, record.y, record.z];
-  };
-
-  recordToVec4 = function(record) {
-    return [record.x, record.y, record.z, record.w];
-  };
-
-  vec3ToRecord = function(vec) {
-    return {
-      x: vec[0],
-      y: vec[1],
-      z: vec[2]
-    };
-  };
-
-  vec4ToRecord = function(vec) {
-    return {
-      x: vec[0],
-      y: vec[1],
-      z: vec[2],
-      w: vec[3]
-    };
-  };
-
-  lookAtToQuaternion = function(lookAt) {
-    var eye, look, up, x, y, z;
-    eye = recordToVec3(lookAt.eye);
-    look = recordToVec3(lookAt.look);
-    up = recordToVec3(lookAt.up);
-    x = [0.0, 0.0, 0.0];
-    y = [0.0, 0.0, 0.0];
-    z = [0.0, 0.0, 0.0];
-    SceneJS_math_subVec3(look, eye, z);
-    SceneJS_math_cross3Vec3(up, z, x);
-    SceneJS_math_cross3Vec3(z, x, y);
-    SceneJS_math_normalizeVec3(x);
-    SceneJS_math_normalizeVec3(y);
-    SceneJS_math_normalizeVec3(z);
-    return SceneJS_math_newQuaternionFromMat3(x.concat(y, z));
-  };
-
-  orbitLookAt = function(dAngles, orbitUp, lookAt) {
-    var axis, dAngle, eye0, eye0len, eye0norm, eye1, look, result, rotMat, tangent0, tangent0norm, tangent1, tangentError, up0, up0norm, up1;
-    if (dAngles[0] === 0.0 && dAngles[1] === 0.0) {
-      return {
-        eye: lookAt.eye,
-        up: lookAt.up
-      };
-    }
-    eye0 = recordToVec3(lookAt.eye);
-    up0 = recordToVec3(lookAt.up);
-    look = recordToVec3(lookAt.look);
-    eye0len = SceneJS_math_lenVec3(eye0);
-    eye0norm = [0.0, 0.0, 0.0];
-    SceneJS_math_mulVec3Scalar(eye0, 1.0 / eye0len, eye0norm);
-    tangent0 = [0.0, 0.0, 0.0];
-    SceneJS_math_cross3Vec3(up0, eye0, tangent0);
-    tangent0norm = SceneJS_math_normalizeVec3(tangent0);
-    up0norm = [0.0, 0.0, 0.0];
-    SceneJS_math_cross3Vec3(eye0norm, tangent0norm, up0norm);
-    axis = [tangent0norm[0] * -dAngles[1] + up0norm[0] * -dAngles[0], tangent0norm[1] * -dAngles[1] + up0norm[1] * -dAngles[0], tangent0norm[2] * -dAngles[1] + up0norm[2] * -dAngles[0]];
-    dAngle = SceneJS_math_lenVec2(dAngles);
-    rotMat = SceneJS_math_rotationMat4v(dAngle, axis);
-    eye1 = SceneJS_math_transformVector3(rotMat, eye0);
-    tangent1 = SceneJS_math_transformVector3(rotMat, tangent0);
-    tangentError = [0.0, 0.0, 0.0];
-    SceneJS_math_mulVec3(tangent1, orbitUp, tangentError);
-    SceneJS_math_subVec3(tangent1, tangentError);
-    up1 = [0.0, 0.0, 0.0];
-    SceneJS_math_cross3Vec3(eye1, tangent1, up1);
-    return result = {
-      eye: vec3ToRecord(eye1),
-      look: lookAt.look,
-      up: vec3ToRecord(up1)
-    };
-  };
-
-  orbitLookAtNode = function(node, dAngles, orbitUp) {
-    return node.set(orbitLookAt(dAngles, orbitUp, {
-      eye: node.get('eye'),
-      look: node.get('look'),
-      up: node.get('up')
-    }));
-  };
-
-  zoomLookAt = function(distance, limits, lookAt) {
-    var eye0, eye0len, eye1, eye1len, look, result;
-    eye0 = recordToVec3(lookAt.eye);
-    look = recordToVec3(lookAt.look);
-    eye0len = SceneJS_math_lenVec3(eye0);
-    if (limits != null) {
-      eye1len = Math.clamp(eye0len + distance, limits[0], limits[1]);
-    } else {
-      eye1len = eye0len + distance;
-    }
-    eye1 = [0.0, 0.0, 0.0];
-    SceneJS_math_mulVec3Scalar(eye0, eye1len / eye0len, eye1);
-    return result = {
-      eye: vec3ToRecord(eye1),
-      look: lookAt.look,
-      up: lookAt.up
-    };
-  };
-
-  zoomLookAtNode = function(node, distance, limits) {
-    return node.set(zoomLookAt(distance, limits, {
-      eye: node.get('eye'),
-      look: node.get('look'),
-      up: node.get('up')
-    }));
   };
 
   modelShaders = function(modelName, shaders) {
