ee586ee2f80cec99b57f6b9a135447f29531d7ca
Rehno Lindeque <errantkid@gmail.com>
2013-04-01 13:06:48 -0600

    Update glQuery to the latest version

--- static/lib/morpheus.complete.js
+++ static/lib/morpheus.complete.js
@@ -29,7 +29,7 @@ var glQuery = (function() {
   // WebGL contexts
   contexts = [],
   // Event callbacks
-  eventCallbacks = { 
+  eventFns = { 
     contextlost: [], 
     contextrestored: [], 
     contextcreationerror: [] 
@@ -133,7 +133,7 @@ var glQuery = (function() {
         || window.mozRequestAnimationFrame
         || window.oRequestAnimationFrame
         || window.msRequestAnimationFrame
-        || function(callback, element){ window.setTimeout(callback, 1000 / 60); };
+        || function(fn, element){ window.setTimeout(fn, 1000 / 60); };
   })();
 
   window.cancelRequestAnimationFrame = (function(){
@@ -1284,15 +1284,16 @@ var glQuery = (function() {
   };
 
 
-  var triggerContextEvents = function(callbacks, event) {
-    for (var i = 0; i < callbacks.length; ++i)
-      if (callbacks[i][1])
-        callbacks[i][0](event);
+  var triggerContextEvents = function(fns, event) {
+    for (var i = 0; i < fns.length; ++i)
+      if (fns[i][1])
+        fns[i][0](event);
   };
 
   // Initialize a WebGL canvas
-  gl.canvas = function(htmlCanvas, contextAttr, width, height) {
-    var canvasId, canvasEl;
+  var canvasExtAPI = {};
+  gl.canvas = function(selector, contextAttr) {
+    var canvasEl, domAttr = domAttr != null? domAttr : {};
     logDebug("canvas");
     var dummy = {
       start: function() { return this; },
@@ -1301,40 +1302,31 @@ var glQuery = (function() {
       clearDepth: function() { return this; },
       clearStencil: function() { return this; }
     };
-    if (typeof htmlCanvas === 'undefined') {
-      // Create canvas element
-      canvasId = 'glqueryCanvas';
-      document.write("<canvas id='" + canvasId + "' width='" + (width != null? width : 800) + "' height='" + (height != null? height : 800) + "'></canvas>");
-      canvasEl = document.getElementById(canvasId);
-    }
-    else {
-      // Get existing canvas element
-      if (!assert(typeof htmlCanvas === 'string' || (typeof htmlCanvas === 'object' && htmlCanvas.nodeName !== 'CANVAS'), "In call to 'canvas', expected type 'string', 'undefined' or 'canvas element' for 'htmlCanvas'. Instead, got type '" + typeof htmlCanvas + "'."))
-        return dummy;
-      canvasId = typeof htmlCanvas === 'string'? htmlCanvas : htmlCanvas.id;
-      canvasEl = typeof htmlCanvas === 'object'? htmlCanvas : document.getElementById(canvasId);
-    }
+    // Get existing canvas element
+    if (!assert(typeof selector === 'string' || (typeof selector === 'object' && (selector.nodeName === 'CANVAS' || selector instanceof WebGLRenderingContext)), "In call to 'canvas', expected type 'string', 'undefined' or 'canvas element' for 'selector'. Instead, got type '" + typeof selector + "'."))
+      return dummy;
+    canvasEl = typeof selector === 'object'? (selector instanceof WebGLRenderingContext? selector.canvas : selector) : document.querySelector(selector);
     if (!assert(canvasEl != null && typeof canvasEl === 'object' && canvasEl.nodeName === 'CANVAS', "In call to 'canvas', could not initialize canvas element."))
       return dummy;
-    if (canvasId != null)
-      logInfo("Initialized canvas: " + canvasId);
+    if (typeof selector === 'string')
+      logInfo("Initialized canvas: " + selector);
     else
       logInfo("Initialized canvas");
 
     // Initialize the WebGL context
-    var canvasCtx = canvasEl.getContext('experimental-webgl', contextAttr);
+    var canvasCtx = selector instanceof WebGLRenderingContext? selector : canvasEl.getContext('experimental-webgl', contextAttr);
     if (!assert(canvasCtx != null, "Could not get a 'experimental-webgl' context."))
       return dummy;
 
     canvasEl.addEventListener("webglcontextlost", function(event) {
       var i;
       // Trigger user events
-      triggerContextEvents(eventCallbacks.contextlost, event);
+      triggerContextEvents(eventFns.contextlost, event);
       // Cancel rendering on all canvases that use request animation frame via
       // gl.canvas(...).start().
       for (i = 0; i < contexts.length; ++i) {
         var context = contexts[i];
-        if (context.ctx.canvas !== canvasEl)
+        if (context.glContext.canvas !== canvasEl)
           continue;
         if (context.nextFrame != null)
           window.cancelAnimationFrame(context.nextFrame);
@@ -1348,61 +1340,61 @@ var glQuery = (function() {
       var i;
       // TODO: reload managed webgl resources
       // Trigger user events
-      triggerContextEvents(eventCallbacks.contextrestored, event);
-      // Resume rendering on all contexts that have not explicitly been paused
-      // via gl.canvas(...).pause().
+      triggerContextEvents(eventFns.contextrestored, event);
+      // Resume rendering on all contexts that have not explicitly been suspended
+      // via gl.canvas(...).suspend().
       for (i = 0; i < contexts.length; ++i) {
         var context = contexts[i];
-        if (context.ctx.canvas !== canvasEl)
+        if (context.glContext.canvas !== canvasEl)
           continue;
-        if (context.nextFrame == null && context.paused === false)
-          window.requestAnimationFrame(context.callback(), context.ctx.canvas);
+        if (context.nextFrame == null && context.suspended === false)
+          window.requestAnimationFrame(context.fnLoop(), context.glContext.canvas);
         break;
       }
     }, false);
 
     canvasEl.addEventListener("webglcontextcreationerror", function(event) {
-      triggerContextEvents(eventCallbacks.contextcreationerror, event);
+      triggerContextEvents(eventFns.contextcreationerror, event);
     }, false);
 
     // Wrap glQuery canvas
     return (function() { 
       var self = { // Private
-        ctx: canvasCtx,
+        glContext: canvasCtx,
         rootId: null,
         nextFrame: null,
-        paused: true,
+        suspended: true,
         clearMask: gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT,
-        callback: function() {
+        fnLoop: function() {
           self = this;
-          return function callback() {
-            if (self.ctx.isContextLost())
+          return function fnLoop() {
+            if (self.glContext.isContextLost())
               return; // Ensure rendering does not continue if context is lost
-            self.ctx.clear(self.clearMask);
-            gl(self.rootId).render(self.ctx);
-            self.nextFrame = window.requestAnimationFrame(callback, self.ctx.canvas);
+            self.glContext.clear(self.clearMask);
+            gl(self.rootId).render(self.glContext);
+            self.nextFrame = window.requestAnimationFrame(fnLoop, self.glContext.canvas);
           };
         }
       };
       // Add context to the global list
       contexts.push(self);
       // Provide context canvas api
-      return { // Public
+      var api = { // Public
         start: function(rootId) {
           logDebug("canvas.start");
           if (rootId != null) {
             if (!assertType(rootId, 'string', 'canvas.start', 'rootId')) return this;
             self.rootId = rootId;
-            self.nextFrame = window.requestAnimationFrame(self.callback(), self.ctx.canvas);
-            self.paused = false;
+            self.nextFrame = window.requestAnimationFrame(self.fnLoop(), self.glContext.canvas);
+            self.suspended = false;
           }
           return this;
         },
-        pause: function() {
-          logDebug("canvas.pause");
-          window.cancelAnimationFrame(self.nextFrame);
+        suspend: function() {
+          logDebug("canvas.suspend");
+          window.cancelAnimationFrame(self.suspend);
           self.nextFrame = null;
-          self.paused = true;
+          self.suspended = true;
           return this;
         },
         clear: function(mask) {
@@ -1412,21 +1404,51 @@ var glQuery = (function() {
         },
         clearColor: function(r,g,b,a) {
           logDebug("canvas.clearColor");
-          self.ctx.clearColor(r,g,b,a);
+          self.glContext.clearColor(r,g,b,a);
           return this;
         },
         clearDepth: function(d) {
           logDebug("canvas.clearDepth");
-          self.ctx.clearDepth(d);
+          self.glContext.clearDepth(d);
           return this;
         },
         clearStencil: function(s) {
           logDebug("canvas.clearStencil");
-          self.ctx.clearStencil(s);
+          self.glContext.clearStencil(s);
+          return this;
+        },
+        viewport: function(x, y, width, height) {
+          logDebug("canvas.viewport");
+          self.glContext.viewport(x, y, width, height);
           return this;
         }
       };
+      for (var k in canvasExtAPI)
+        if (!(k in api)) // Don't override the base api
+          api[k] = canvasExtAPI[k](self);
+      return api;
     })();
+  };
+
+  gl.canvas.extend = function(name, fn) {
+    logDebug("canvas.extend (" + name + ")");
+    if (!assertType(name, 'string', 'canvas.extend', 'name')) return this;
+    if (!assertType(fn, 'function', 'canvas.extend', 'fn')) return this;
+    canvasExtAPI[name] = fn;
+    return gl.canvas;
+  };
+
+  gl.canvas.create = function(id, contextAttr, domAttr) {
+    logDebug("canvas.create (#" + id + ")");
+    var id = (id == null)? 'glqueryCanvas' : id;
+    if (!assertType(id, 'string', 'canvas.create', 'id')) return this;
+    var newDomAttr = { id: id, width: 800, height: 800 };
+    for (var k in domAttr)
+      newDomAttr[k] = domAttr[k];
+    var canvasEl = document.createElement('canvas');
+    Object.keys(newDomAttr).map(function(k){ canvasEl.setAttribute(k, newDomAttr[k]); }).join(' ');    
+    document.write(canvasEl.outerHTML);
+    return gl.canvas("#" + id, contextAttr);
   };
 
   // Create a glQuery scene hierarchy
@@ -1495,30 +1517,30 @@ var glQuery = (function() {
     return gl;
   };
 
-  var registerContextEvent = function(eventName, callback, active) {
+  var registerContextEvent = function(eventName, fn, active) {
     var i, active = active;
     // Clear the list of callbacks if nothing was passed in
-    if(typeof callback === 'undefined') {
-      eventCallbacks[eventName] = [];
+    if(typeof fn === 'undefined') {
+      eventFns[eventName] = [];
       return;
     }
     // Check that callback is a function and active is a boolean
-    assertType(callback, 'function', eventName, 'callback');
+    assertType(fn, 'function', eventName, 'fn');
     typeof active !== 'undefined' && assertType(active, 'boolean', eventName, 'active');
     // Prevent the same callback from being added to the list twice.
     active = active === false? active : true;
-    for (i = 0; i < eventCallbacks[eventName].length; ++i)
-      if (eventCallbacks[eventName][i][0] === callback) {
-        eventCallbacks[eventName][i][1] = active;
+    for (i = 0; i < eventFns[eventName].length; ++i)
+      if (eventFns[eventName][i][0] === fn) {
+        eventFns[eventName][i][1] = active;
         return;
       }
-    // Add the callback
-    eventCallbacks[eventName].push([callback, active]);
+    // Add the event callback
+    eventFns[eventName].push([fn, active]);
   };
   
-  gl.contextlost = function(callback, active) { registerContextEvent('contextlost',callback,active); };
-  gl.contextrestored = function(callback, active) { registerContextEvent('contextrestored',callback,active); };
-  gl.contextcreationerror = function(callback) { registerContextEvent('contextcreationerror',callback,active); };
+  gl.contextlost = function(fn, active) { registerContextEvent('contextlost',fn,active); };
+  gl.contextrestored = function(fn, active) { registerContextEvent('contextrestored',fn,active); };
+  gl.contextcreationerror = function(fn) { registerContextEvent('contextcreationerror',fn,active); };
 
 
   gl.worker = function(workerId, js) {
@@ -1528,9 +1550,10 @@ var glQuery = (function() {
 
   // Export glQuery to a CommonJS module if exports is available
   if (typeof(exports) !== "undefined" && exports !== null)
-    exports.glQuery = gl;
+    exports.gl = exports.glQuery = gl;
   return gl;
 })();
+var gl = typeof gl === 'undefined'? glQuery : gl;
 
 
 
